989001b9be56ee5f016bf62051595640549c51bd
isaacgalvan10 <galvan1522@gmail.com>
2020-08-03 18:40:21 -0500

    Initialize project

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import React, { Component, Fragment } from 'react';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <Fragment>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#">Why Saleswhale?</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Product</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Company</a>
+              </li>
+            </ul>
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </div>
+        </nav>
+      </Fragment>
+    );
+  }
+}
+
+export default NavBar;
