0e4365a2d2dd5139517d8f1e0c7afb6820427361
isaacgalvan10 <galvan1522@gmail.com>
2020-08-15 09:41:35 -0500

    fixing animations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { TweenMax } from 'gsap';
 import NavBar from './components/NavBar';
 import Main from './components/Main';
 import Footer from './components/Footer';
@@ -6,8 +7,15 @@ import Animations from './js/Animations';
 import './App.css';
 
 function App() {
+  let app = useRef(null);
+
+  useEffect(() => {
+    TweenMax.to(app, 0, { css: { visibility: 'visible' } })
+    console.log(app)
+  })
+
   return (
-    <div className="App">
+    <div className="App" ref={el => app = el}>
       <Animations />
       <NavBar />
       <Main />
